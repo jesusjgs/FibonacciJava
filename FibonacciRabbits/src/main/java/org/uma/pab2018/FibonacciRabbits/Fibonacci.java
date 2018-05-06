c6e947f45832d7b7be76b16a4cebaa4fc9422680
jesusjgs <jesusjgs@alu.uma.es>
2018-05-06 23:43:59 +0200

    Tests and limitation on values added

--- FibonacciRabbits/src/main/java/org/uma/pab2018/FibonacciRabbits/Fibonacci.java
+++ FibonacciRabbits/src/main/java/org/uma/pab2018/FibonacciRabbits/Fibonacci.java
@@ -4,12 +4,17 @@ public class Fibonacci {
 
 	public int Compute(int n, int k, int earlier, int beforeThanEarlier) {
 		int res;
+		if(n>40 || n<0 || k>5 || k<0) {
+			throw new RuntimeException("Error introducing data");
+		}
+		else {
 		if (n > 1) {
 			res = Compute(n - 1, k, earlier + beforeThanEarlier * k, earlier);
 		} else {
 			res = earlier;
 		}
 		return res;
+	}
 	}
 
 }
--- FibonacciRabbits/src/test/java/org/uma/pab2018/FibonacciRabbits/FibonacciTest.java
+++ FibonacciRabbits/src/test/java/org/uma/pab2018/FibonacciRabbits/FibonacciTest.java
@@ -18,11 +18,29 @@ public class FibonacciTest {
 	
 	@Test 
 	public void shouldFibRabOfNOneReturnOne() {
-		int result = fibonacci.Compute(5,3,1,0);
-		int expectedResult=19;
+		int result = fibonacci.Compute(1,1,1,0);
+		int expectedResult=1;
 		
 		assertEquals(expectedResult,result);
 	}
+	
+	@Test (expected= RuntimeException.class)
+	public void shouldNValueLess41() {
+		fibonacci.Compute(41,1,1,0);
+	}
+	@Test (expected= RuntimeException.class)
+	public void shouldNValuePossitive() {
+		fibonacci.Compute(-1,1,1,0);
+	}
+	@Test (expected= RuntimeException.class)
+	public void shouldKValueLess6() {
+		fibonacci.Compute(2,6,1,0);
+	}
+	@Test (expected= RuntimeException.class)
+	public void shouldKValuePositive() {
+		fibonacci.Compute(2,-1,1,0);
+	}
+	
 	
 	@Test
 	public void shouldFibRabOfNFiveKThreeReturnNineteen() {
