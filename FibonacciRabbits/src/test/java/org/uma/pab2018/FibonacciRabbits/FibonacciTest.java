64589ddada8ff19ae49f3e8f2df316d1594e6de7
jesusjgs <jesusjgs@alu.uma.es>
2018-05-06 23:45:37 +0200

    Formated source code

--- FibonacciRabbits/src/test/java/org/uma/pab2018/FibonacciRabbits/FibonacciTest.java
+++ FibonacciRabbits/src/test/java/org/uma/pab2018/FibonacciRabbits/FibonacciTest.java
@@ -2,53 +2,54 @@ package org.uma.pab2018.FibonacciRabbits;
 
 import static org.junit.Assert.assertEquals;
 
-
 import org.junit.Before;
 import org.junit.Test;
 
 public class FibonacciTest {
 
 	private Fibonacci fibonacci;
-	
+
 	@Before
-	
+
 	public void init() {
-		fibonacci= new Fibonacci();
+		fibonacci = new Fibonacci();
 	}
-	
-	@Test 
+
+	@Test
 	public void shouldFibRabOfNOneReturnOne() {
-		int result = fibonacci.Compute(1,1,1,0);
-		int expectedResult=1;
-		
-		assertEquals(expectedResult,result);
+		int result = fibonacci.Compute(1, 1, 1, 0);
+		int expectedResult = 1;
+
+		assertEquals(expectedResult, result);
 	}
-	
-	@Test (expected= RuntimeException.class)
+
+	@Test(expected = RuntimeException.class)
 	public void shouldNValueLess41() {
-		fibonacci.Compute(41,1,1,0);
+		fibonacci.Compute(41, 1, 1, 0);
 	}
-	@Test (expected= RuntimeException.class)
+
+	@Test(expected = RuntimeException.class)
 	public void shouldNValuePossitive() {
-		fibonacci.Compute(-1,1,1,0);
+		fibonacci.Compute(-1, 1, 1, 0);
 	}
-	@Test (expected= RuntimeException.class)
+
+	@Test(expected = RuntimeException.class)
 	public void shouldKValueLess6() {
-		fibonacci.Compute(2,6,1,0);
+		fibonacci.Compute(2, 6, 1, 0);
 	}
-	@Test (expected= RuntimeException.class)
+
+	@Test(expected = RuntimeException.class)
 	public void shouldKValuePositive() {
-		fibonacci.Compute(2,-1,1,0);
+		fibonacci.Compute(2, -1, 1, 0);
 	}
-	
-	
+
 	@Test
 	public void shouldFibRabOfNFiveKThreeReturnNineteen() {
 
-		int result = fibonacci.Compute(5,3,1,0);
-		int expectedResult=19;
-		
-		assertEquals(expectedResult,result);
+		int result = fibonacci.Compute(5, 3, 1, 0);
+		int expectedResult = 19;
+
+		assertEquals(expectedResult, result);
 	}
-	
+
 }
